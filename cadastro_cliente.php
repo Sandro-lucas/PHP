6bf2e980a364971c64a7962e48a979be8761dc87
Sandro-lucas <sandrolucasekell@gmail.com>
2024-05-03 15:51:31 -0300

    dvs

--- cadastro_cliente.php
+++ cadastro_cliente.php
@@ -7,24 +7,24 @@ include_once "head.php";
 <br><CENTER>
 <div class="form-ui"><CENTER>
     <h1>CADASTRO</h1><br><br>
-<form action="functions/function_cadastrar.php" method="post">
+<form action="functions/functions_cadastrar.php" method="post">
 
     
 <div class="form-group row">
         <label for="inputText" class="col-sm-2 col-form-label">Nome</label>
         <div class="col-sm-10">
-            <input type="text" class="form-control" id="Nome"><br><br>
+            <input type="text" class="form-control" id="Nome" name="Nome"><br><br>
         </div>
 
 
         <label for="inputText" class="col-sm-2 col-form-label">Telefone</label>
         <div class="col-sm-10">
-            <input type="text" class="form-control" id="Telefone"><br><br>
+            <input type="text" class="form-control" id="Telefone" name="Telefone"><br><br>
         </div>
 
         <label for="inputText" class="col-sm-2 col-form-label">E-mail</label>
         <div class="col-sm-10">
-            <input type="text" class="form-control" id="E-mail"><br><br>
+            <input type="text" class="form-control" id="E-mail" name="E-mail"><br><br>
         </div>
         <button class="btn_01 btn-primary" type="submit">Cadastrar</button><br>
         <button class="btn_01  btn-secondary" type="reset">Limpar</button>
---
+++ functions/functions_cadastrar.php
@@ -0,0 +1,23 @@
+<?php 
+
+    include_once "../bd/conexao.php";
+
+
+    $nome = $_POST['Nome'];
+    $telefone = $_POST['Telefone'];
+    $email = $_POST['E-mail'];
+   
+   
+ $sql ="INSERT INTO clientes (nome,telefone,email) values ('$nome','$telefone', '$email')";
+
+$resutado = mysqli_query($conn, $sql);
+
+if($resutado) {
+echo " <script>alert('Registro salvo com sucesso')</script>";
+}else{
+    " <script>alert('Erro ao salvar com registro')</script>";
+}
+
+
+
+?>
